aa9b8c599d85069af848be818069ad9c15666cf3
Omar Suleiman <jb18940@bristol.ac.uk>
2020-05-22 17:37:11 +0200

    reformat rendering

---
+++ entity.py
@@ -0,0 +1,13 @@
+class Entity:
+    """
+    Object to represent anything in the game world
+    """
+    def __init__(self, x, y, char, colour):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.colour = colour
+
+    def move(self, dx, dy):
+        self.x += dx
+        self.y += dy
