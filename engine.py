8a593496b1759a797e25bc82927720d3f14739a1
Omar Suleiman <jb18940@bristol.ac.uk>
2020-05-22 02:46:23 +0200

    keys

---
+++ engine.py
@@ -0,0 +1,31 @@
+import tcod as libtcod
+
+
+def main():
+    screen_width = 80
+    screen_height = 50
+
+    player_x = int(screen_width/2)
+    player_y = int(screen_height/2)
+
+    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+
+    libtcod.console_init_root(screen_width, screen_height, 'Omars Roguelike', False)
+
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
+    while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+        libtcod.console_set_default_foreground(0, libtcod.white)
+        libtcod.console_put_char(0, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_flush()
+
+        action = handle_keys(key)
+
+        if key.vk == libtcod.KEY_ESCAPE:
+            return True
+
+
+if __name__ == '__main__':
+    main()
